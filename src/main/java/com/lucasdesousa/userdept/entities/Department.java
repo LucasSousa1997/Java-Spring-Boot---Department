6e1706287705ea011f437a332037380eb906981e
LucasDeSousa <lucassousa1710@hotmail.com>
2023-01-02 18:38:04 -0300

    Primeiro projeto Spring Boot

---
+++ src/main/java/com/lucasdesousa/userdept/entities/Department.java
@@ -0,0 +1,37 @@
+package com.lucasdesousa.userdept.entities;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "tb_department")
+
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Department(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+
+
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_department(name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('Informática');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Lucas', 'Lucas@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Lena', 'lena@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Sophia', 'sophia@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Gustavo', 'gustavo@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Thais', 'thais@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Mixe', 'mixe@gmail.com');
